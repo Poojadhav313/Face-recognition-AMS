f82eb1f270ea12575cc50a4bc1c5a2ea30e65035
Pooja <poojadhav313@gmail.com>
2024-03-16 00:27:14 +0530

    recognize

--- capture.py
+++ capture.py
@@ -1,32 +1,91 @@
-import cv2
 import face_recognition
+import cv2
+import numpy as np
+
+# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
+# other example, but it includes some basic performance tweaks to make things run a lot faster:
+#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
+#   2. Only detect faces in every other frame of video.
 
 
+cp = cv2.VideoCapture(0)
 
-cap = cv2.VideoCapture(0)   #default webcam : 0
 
-if not cap.isOpened():
-    print("Error capturing video")
-    exit()
+obama_image = face_recognition.load_image_file("images/98374.jpg")
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+my_image = face_recognition.load_image_file("images/me.jpg")
+my_face_encoding = face_recognition.face_encodings(my_image)[0]
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [
+    obama_face_encoding,
+    my_face_encoding
+]
+known_face_names = [
+    "Barack Obama",
+    "Pooja jadhav"
+]
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
 
 while True:
-    boolval, frame1 = cap.read()
+    ret, frame1 = cp.read()
+    frame = cv2.flip(frame1,1)  
 
-    if not boolval:
-        print("No frame captured")
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+        
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+
+
+            # use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+
+            face_names.append(name)
+
+    process_this_frame = not process_this_frame
+
+
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top-30), (right, bottom+30), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom), (right, bottom+30), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom + 20), font, 0.6, (255, 255, 255), 1)
+ 
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) == ord('q'):
         break
 
-    frame = cv2.flip(frame1, 1)
-    cv2.imshow("frame", frame)
-
-    key = cv2.waitKey(1) #wait for key pressed and stores ascii. if not any key pressed returns -1
-    print(key)
-
-    if key == ord('q'):  
-        break
-
-cap.release()
+cp.release()
 cv2.destroyAllWindows()
-
-print("Video capturing closed")
-
