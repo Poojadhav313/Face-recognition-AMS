dc91973f470d4412d05297ccc17a8afeb755789e
Pooja <poojadhav313@gmail.com>
2024-04-06 20:20:09 +0530

    DB get set data

--- FaceRecognition/recognize/urls.py
+++ FaceRecognition/recognize/urls.py
@@ -2,7 +2,7 @@ from django.urls import URLPattern, path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.temp, name="home"),
     path('add/', views.addData),
     
     ]
--- FaceRecognition/recognize/views.py
+++ FaceRecognition/recognize/views.py
@@ -13,6 +13,78 @@ def home(request):
     return render(request, "recognize/home.html")
 
 
+def temp(request):
+    collection = DBtable
+    cursor = collection.find()
+    known_encodings = []
+    known_names = []
+    for document in cursor:
+        known_encodings.append(document["encoding"])
+        known_names.append(document["name"])
+
+    # Initialize video capture
+    cp = cv2.VideoCapture(0)
+
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    process_this_frame = True
+
+    while True:
+        ret, frame1 = cp.read()
+        frame = cv2.flip(frame1, 1)
+
+        # Only process every other frame of video to save time
+        if process_this_frame:
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+            # Convert the image from BGR color to RGB color
+            rgb_small_frame = small_frame[:, :, ::-1]
+
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(known_encodings, face_encoding)
+                name = "Unknown"
+
+                # use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(known_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_names[best_match_index]
+
+                face_names.append(name)
+
+        process_this_frame = not process_this_frame
+
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top - 30), (right, bottom + 30), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom), (right, bottom + 30), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom + 20), font, 0.6, (255, 255, 255), 1)
+
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cp.release()
+    cv2.destroyAllWindows()
+    return HttpResponse("running")
+
 def addData(request):  
 
     face = {}
